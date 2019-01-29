6da9e3fd3db1fbb7c4caa8368a7c1c0b5592adb0
VIISilver <jparke71@hotmail.com>
2019-01-28 23:59:44 -0700

    Added Basic Home page

---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Nav from '../../functional/Nav';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Jello World!</h1>
+        <Nav />
+      </div>
+    )
+  }
+}
+
+export default Home;
--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 
 import About from './components/about/About.js';
+import Home from './components/home/Home.js';
 
 export default (
     <HashRouter>
         <div>
+            <Route component={Home} exact path='/' />
             <Route component={About} path='/about' />
         </div>
     </HashRouter>
